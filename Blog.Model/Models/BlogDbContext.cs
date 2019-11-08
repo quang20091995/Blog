066dab144e08c7564e0e1837a31230726481e0b7
quang20091995 <31998955+quang20091995@users.noreply.github.com>
2019-11-08 12:17:05 +0900

    Update Identity Role, Identity User to Database

--- Blog.Model/Models/BlogDbContext.cs
+++ Blog.Model/Models/BlogDbContext.cs
@@ -1,11 +1,26 @@
 ﻿namespace Blog.Model.Models
 {
     using Microsoft.EntityFrameworkCore;
-    public class BlogDbContext: DbContext
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    public class BlogDbContext: IdentityDbContext<IdentityUser>
     {
-        public BlogDbContext(DbContextOptions options) : base(options)
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
         {
 
+        }
+
+        // Creatin Roles for or application
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>().HasData(
+                    new { Id = "1", Name = "Admin", NormalizedName = "ADMIN" },
+                    new { Id = "2", Name = "Customer", NormalizedName = "CUSTOMER" },
+                    new { Id = "3", Name = "Moderator", NormalizedName = "MODERATOR" }
+                );
         }
 
         DbSet<Article> Articles { get; set; }
--- Blog/Startup.cs
+++ Blog/Startup.cs
@@ -1,16 +1,22 @@
 namespace Blog
 {
+    using Blog.Helper;
     using Blog.Infrastructure;
     using Blog.Model.Models;
-
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.HttpsPolicy;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.SpaServices.AngularCli;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.IdentityModel.Tokens;
+    using System;
+    using System.Text;
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -23,7 +29,7 @@ namespace Blog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<BlogDbContext>(item => item.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), target => target.MigrationsAssembly("Blog.Model")));
+            services.AddDbContext<BlogDbContext>(item => item.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), target => target.MigrationsAssembly("Blog")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
@@ -31,6 +37,67 @@ namespace Blog
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
+            });
+
+            // Enable CORS
+            services.AddCors(options =>
+            {
+                options.AddPolicy("EnableCORS", builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowCredentials().Build();
+                });
+            });
+
+            // Specifiying we are going to use Identity Framework
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = true;
+                options.User.RequireUniqueEmail = true;
+                // Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+            }).AddEntityFrameworkStores<BlogDbContext>().AddDefaultTokenProviders();
+
+            // Configure strongly typed settings objects
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+
+            // Authentication Middleware
+            services.AddAuthentication(o =>
+            {
+                o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                o.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+                o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidIssuer = appSettings.Site,
+                    ValidAudience = appSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+            });
+
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("RequireLoggedIn", policy => policy.RequireRole("Admin", "Customer", "Moderator").RequireAuthenticatedUser());
+
+                options.AddPolicy("RequireAdministratorRole", policy => policy.RequireRole("Admin").RequireAuthenticatedUser());
             });
         }
 
@@ -53,6 +120,8 @@ namespace Blog
             app.UseSpaStaticFiles();
 
             app.UseMvcWithAreas();
+
+            app.UseCors("EnableCORS");
 
             app.UseSpa(spa =>
             {
